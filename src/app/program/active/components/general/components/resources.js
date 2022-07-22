032db54ca7f05aa8fb916223272ef6b1441a22ed
mukeshMukesh12 <mukesh.k2@gai.co.in>
2022-07-22 14:37:30 +0530

    added image tag in resources section

--- src/app/program/active/components/general/components/resources.js
+++ src/app/program/active/components/general/components/resources.js
@@ -245,13 +245,13 @@ const Resources = () => {
 
       <div className='flex w-full justify-end'>
         <AttachQuiz addQuiz={addQuiz} />
-        {/* <Button
+        <Button
           type='submit'
           className='mt-2 inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
           onClick={() => setUploadType('image')}
         >
           Add Image
-        </Button> */}
+        </Button>
         <Button
           type='submit'
           className='mt-2 inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
